21411dd1b66b39a04f1ab2691e425175c1414387
Prashantbhalla1 <82282194+Prashantbhalla1@users.noreply.github.com>
2023-05-29 16:57:24 +0530

    Update mongoose.js

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 const validator=require('validator')
 console.log(process.env.MONGOURL)
-mongoose.connect('mongodb://127.0.0.1:27017/mathquiz',{
+mongoose.connect(process.env.MONGOURL,{
     useNewUrlParser:true,
     
 })
